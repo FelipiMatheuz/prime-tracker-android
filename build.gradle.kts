598f564c457c6dd914711872618118700bca6a06
Felipi Matheuz <felipi568@gmail.com>
2026-07-23 13:30:45 -0300

    Updated dependency versions migrated plugins id to alias (modern) added Room dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.2.1" apply false
-    id("com.android.library") version "9.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
-    id("com.google.gms.google-services") version "4.5.0" apply false
-    id("com.google.dagger.hilt.android") version "2.60.1" apply false
-    id("com.google.devtools.ksp") version "2.3.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.google.services) apply false
 }
