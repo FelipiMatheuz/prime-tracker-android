990dcd35316fe24ae74ac4ccd4501c5dda1f8432
Felipi Matheuz <felipi568@gmail.com>
2026-07-25 16:34:07 -0300

    Minor refactors in PrimeSetScreen.kt and PrimeDetailScreen.kt

--- app/src/main/java/com/felipimatheuz/primehunt/data/remote/enums/DropRarity.kt
+++ app/src/main/java/com/felipimatheuz/primehunt/data/remote/enums/DropRarity.kt
@@ -1,7 +1,23 @@
 package com.felipimatheuz.primehunt.data.remote.enums
 
-enum class DropRarity {
-    COMMON,
-    UNCOMMON,
-    RARE
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.felipimatheuz.primehunt.ui.theme.Common
+import com.felipimatheuz.primehunt.ui.theme.CommonDark
+import com.felipimatheuz.primehunt.ui.theme.Rare
+import com.felipimatheuz.primehunt.ui.theme.RareDark
+import com.felipimatheuz.primehunt.ui.theme.Uncommon
+import com.felipimatheuz.primehunt.ui.theme.UncommonDark
+
+enum class DropRarity(val lightColor: Color, val darkColor: Color) {
+    COMMON(Common, CommonDark),
+    UNCOMMON(Uncommon, UncommonDark),
+    RARE(Rare, RareDark);
+
+    @Composable
+    fun getColor(): Color {
+        val isDark = isSystemInDarkTheme()
+        return if (isDark) darkColor else lightColor
+    }
 }
--- app/src/main/java/com/felipimatheuz/primehunt/data/remote/enums/RelicSource.kt
+++ app/src/main/java/com/felipimatheuz/primehunt/data/remote/enums/RelicSource.kt
@@ -1,8 +1,10 @@
 package com.felipimatheuz.primehunt.data.remote.enums
 
-enum class RelicSource {
-    MISSION,
-    RESURGENCE,
-    BARO,
-    VAULT
+import com.felipimatheuz.primehunt.R
+
+enum class RelicSource(val displayNameRes: Int) {
+    MISSION(R.string.status_mission),
+    RESURGENCE(R.string.status_resurgence),
+    BARO(R.string.status_baro),
+    VAULT(R.string.status_vault)
 }
