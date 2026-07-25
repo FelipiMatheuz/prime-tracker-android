d78ee74ce115488f2a2cb52a3533309efbfde409
Felipi Matheuz <felipi568@gmail.com>
2026-07-25 14:50:08 -0300

    Window adjustments for Android 15+

--- app/src/main/java/com/felipimatheuz/primehunt/ui/theme/Theme.kt
+++ app/src/main/java/com/felipimatheuz/primehunt/ui/theme/Theme.kt
@@ -1,6 +1,9 @@
 package com.felipimatheuz.primehunt.ui.theme
 
 import android.app.Activity
+import android.os.Build
+import android.view.Window
+import android.view.WindowInsets
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -41,7 +44,7 @@ fun PrimeTrackerTheme(
         SideEffect {
             val window = (view.context as Activity).window
             WindowCompat.setDecorFitsSystemWindows(window, true)
-            window.statusBarColor = colorScheme.primary.toArgb()
+            setStatusBarColor(window, colorScheme.primary.toArgb())
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
@@ -51,4 +54,17 @@ fun PrimeTrackerTheme(
         typography = Typography,
         content = content
     )
+}
+
+private fun setStatusBarColor(window: Window, color: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+        window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsets.Type.statusBars())
+            view.setBackgroundColor(color)
+            view.setPadding(0, statusBarInsets.top, 0, 0)
+            insets
+        }
+    } else {
+        window.statusBarColor = color
+    }
 }
